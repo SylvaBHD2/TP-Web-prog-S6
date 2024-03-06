981afd2050e21d406cb59f80f223d3edfc8a5d2c
SylvaBHD2 <svat.buhard@gmail.com>
2024-03-06 20:01:49 +0100

    Add processDataF function to process CSV data and print the first 10 objects

--- TP2/Ex 4/ex.js
+++ TP2/Ex 4/ex.js
@@ -1,63 +1,3 @@
-
-// function processData (csvText) {
-//     // get every line and put them in object
-//     var lines = csvText.split("\n");
-//     var data = [];
-//     for (var i = 0; i < lines.length; i++) {
-//         var line = lines[i];
-//         console.log(line);
-//         var cells = line.split(",");
-//         console.log(cells);
-//         var website2 = cells[2] === "" ? null : cells[2];
-//         console.log("Test here : ",cells[0]," | ",cells[1], " | ", website2," | ");
-//         var obj = { username: cells[0], realName: cells[1], website: website2, projectName: cells[4] };
-//         data.push(obj);
-//     }
-//     //print the 10 first objects
-//     // for (var i = 0; i < 10; i++) {
-//     //     console.log(data[i]);
-//     // }
-//     return data;
-// }
-
-
-
-// function processDataImperative (csvText) {
-//     // get every line and put them in object
-//     var lines = csvText.split("\n");
-//     var data = [];
-//     for (var i = 0; i < lines.length; i++) {
-//         var line = lines[i];
-//         var cells = line.split(",");
-//         var website = cells[2] === "" ? null : cells[2];
-//         var obj = { username: cells[0], realName: cells[1], website: website, projectName: cells[4] };
-//         data.push(obj);
-//     }
-//     //print the 10 first objects
-//     for (var i = 0; i < 10; i++) {
-//         console.log(data[i]);
-//     }
-//     return data;
-// }
-
-// function processDataFunctionnal(csvText){
-//     const lines = csvText.split("\n");
-//     const data = [];
-//     for (const i = 0; i < lines.length; i++) {
-//         const line = lines[i];
-//         const cells = line.split(",");
-//         var website = cells.filter(x => x!== "")[2];
-//         var obj = { username: cells[0], realName: cells[1], website: website, projectName: cells[4] };
-//         data.push(obj);
-//     }
-//     //print the 10 first objects
-//     for (var i = 0; i < 10; i++) {
-//         console.log(data[i]);
-//     }
-//     return data;
-// }
-
-
 const fs = require('fs');
 
 function processData(csvText) {
@@ -77,21 +17,41 @@ function processData(csvText) {
         console.log(data[i]);
     }
     return data;
-}
+};
 
-// Read the CSV file
-fs.readFile('apache_people_projects.csv', 'utf8', (err, csvText) => {
+function processDataF(csvText) {
+    // get every line and put them in object
+    const lines = csvText.split("\n");
+    const data = lines.map(line => {
+        const cells = line.split(",");
+        // console.log("cells", cells);
+        const website = cells[2] === "" ? null : cells[2];
+        return { username: cells[0], realName: cells[1], website: website, projectName: cells[3] };
+    });
+    data.slice(0, 3).forEach(obj => console.log(obj)); // Print the first 10 objects
+    return data;
+};
+
+
+
+// // Read the CSV file
+// fs.readFile('./Ex 4/apache_people_projects.csv', 'utf8', (err, csvText) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     // Process the CSV data
+//     processData(csvText);
+// });
+
+// console.log("WHATISTHAAAAAAAT Functionnal now");
+// 
+fs.readFile('./Ex 4/apache_people_projects.csv', 'utf8', (err, csvText) => {
     if (err) {
         console.error(err);
         return;
     }
     // Process the CSV data
-    processData(csvText);
+    processDataF(csvText);
 });
-
-
-
-processData("apache_people_projects.csv");
-// processDataImperative("apache_people_projects.csv");
-// processDataFunctionnal("apache_people_projects.csv");
 
